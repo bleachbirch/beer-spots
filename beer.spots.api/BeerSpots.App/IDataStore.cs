d1cb8ffe91f1068dfbe0edc20653f3f54037bf68
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-04-16 20:47:37 +0300

    added interfaces of buisness logic and db interaction

---
+++ beer.spots.api/BeerSpots.App/IDataStore.cs
@@ -0,0 +1,58 @@
+﻿using BeerSpots.Data.Entities;
+
+namespace BeerSpots.App
+{
+    /// <summary>
+    /// Interface for interaction with database
+    /// </summary>
+    public interface IDataStore
+    {
+        /// <summary>
+        /// Returns IQueryable object with spot's entities
+        /// </summary>
+        /// <returns></returns>
+        Task<IQueryable<Spot>> GetAllAsync();
+
+        /// <summary>
+        /// Get spot by id or null, if not found
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Spot?> GetAsync(long id);
+
+        /// <summary>
+        /// Returns spot by coordinate or null
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        Task<Spot?> GetAsync(Coordinate coordinate);
+
+        /// <summary>
+        /// Adds new spot to store
+        /// </summary>
+        /// <param name="spot"></param>
+        /// <returns></returns>
+        Task AddAsync(Spot spot);
+
+        /// <summary>
+        /// Updates exists spot
+        /// </summary>
+        /// <param name="spot"></param>
+        /// <returns></returns>
+        Task UpdateAsync(Spot spot);
+
+        /// <summary>
+        /// Deletes spot by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteAsync(long id);
+
+        /// <summary>
+        /// Deletes spot by coordinate
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteAsync(Coordinate coordinate);
+    }
+}
---
+++ beer.spots.api/BeerSpots.App/ISpotInteractor.cs
@@ -0,0 +1,46 @@
+﻿using BeerSpots.Data.Entities;
+
+namespace BeerSpots.App
+{
+    /// <summary>
+    /// Interface of app
+    /// </summary>
+    public interface ISpotInteractor
+    {
+        /// <summary>
+        /// Returns nearest spots in specified radius. Returns null, if spots are not founded.
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Spot?>> GetAsync(Coordinate coordinate, int radius);
+
+        /// <summary>
+        /// Returns one spot by his coordinate. Return null, if spot is not founded.
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        Task<Spot?> GetSpotAsync(Coordinate coordinate);
+
+        /// <summary>
+        /// Creates spot
+        /// </summary>
+        /// <param name="newSpot"></param>
+        /// <returns></returns>
+        Task CreateAsync(Spot newSpot);
+
+        /// <summary>
+        /// Edites spot
+        /// </summary>
+        /// <param name="spot"></param>
+        /// <returns></returns>
+        Task EditAsync(Spot spot);
+
+        /// <summary>
+        /// Deletes spot
+        /// </summary>
+        /// <param name="spot"></param>
+        /// <returns></returns>
+        Task DeleteAsync(Spot spot);
+    }
+}
