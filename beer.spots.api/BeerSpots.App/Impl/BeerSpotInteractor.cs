30a58691430539d271899f4c06be0247264cd875
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-04-17 09:55:35 +0300

    fix spot interactor's implementation

--- beer.spots.api/BeerSpots.App/Impl/BeerSpotInteractor.cs
+++ beer.spots.api/BeerSpots.App/Impl/BeerSpotInteractor.cs
@@ -1,30 +1,30 @@
-﻿using BeerSpots.Data.Entities;
+﻿using BeerSpots.App.Models;
 
 namespace BeerSpots.App.Impl
 {
     internal class BeerSpotInteractor : ISpotInteractor
     {
-        public Task CreateAsync(Spot newSpot)
+        public Task CreateAsync(SpotDto newSpot)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(Spot spot)
+        public Task DeleteAsync(SpotDto spot)
         {
             throw new NotImplementedException();
         }
 
-        public Task EditAsync(Spot spot)
+        public Task EditAsync(SpotDto spot)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Spot?>> GetAsync(Coordinate coordinate, int radius)
+        public Task<IEnumerable<SpotDto?>> GetAsync(CoordinateDto coordinate, int radius)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Spot?> GetSpotAsync(Coordinate coordinate)
+        public Task<SpotDto?> GetSpotAsync(CoordinateDto coordinate)
         {
             throw new NotImplementedException();
         }
