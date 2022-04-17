7bccd12aa509f1ebf36f3e39a590df3763ee62cd
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-04-17 09:33:17 +0300

    added latitude and longitude properties to coordinate

--- beer.spots.api/BeerSpots.Data.Entities/Coordinate.cs
+++ beer.spots.api/BeerSpots.Data.Entities/Coordinate.cs
@@ -8,6 +8,18 @@
         public long Id { get; set; }
 
         /// <summary>
+        /// Latitude
+        /// TODO: type decimal(8, 6)
+        /// </summary>
+        public decimal Latitude { get; set; }
+
+        /// <summary>
+        /// Longitude
+        /// TODO: type decimal(9, 6)
+        /// </summary>
+        public decimal Longitude { get; set; }
+
+        /// <summary>
         /// Which spot is associated with this
         /// </summary>
         public Spot Spot { get; set; }
