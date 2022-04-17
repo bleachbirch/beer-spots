ccdee9d428750a9b4d9070ef48756e125307f309
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-04-17 10:19:18 +0300

    implemented methods of BeerSpotInteractor

--- beer.spots.api/BeerSpots.App/Impl/BeerSpotInteractor.cs
+++ beer.spots.api/BeerSpots.App/Impl/BeerSpotInteractor.cs
@@ -1,32 +1,50 @@
-﻿using BeerSpots.App.Models;
+﻿using AutoMapper;
+using BeerSpots.App.Models;
+using BeerSpots.Data.Entities;
 
 namespace BeerSpots.App.Impl
 {
     internal class BeerSpotInteractor : ISpotInteractor
     {
-        public Task CreateAsync(SpotDto newSpot)
+        private readonly IDataStore _dataStore;
+        private readonly IMapper _mapper;
+
+        public BeerSpotInteractor(IDataStore dataStore, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _dataStore = dataStore;
+            _mapper = mapper;
         }
 
-        public Task DeleteAsync(SpotDto spot)
+        public async Task CreateAsync(SpotDto newSpot)
         {
-            throw new NotImplementedException();
+            var spot = _mapper.Map<Spot>(newSpot);
+            await _dataStore.AddAsync(spot);
         }
 
-        public Task EditAsync(SpotDto spot)
+        public async Task DeleteAsync(SpotDto spot)
         {
-            throw new NotImplementedException();
+            var coordinate = _mapper.Map<Coordinate>(spot.Сoordinate);
+            await _dataStore.DeleteAsync(coordinate);
         }
 
-        public Task<IEnumerable<SpotDto?>> GetAsync(CoordinateDto coordinate, int radius)
+        public async Task EditAsync(SpotDto spot)
         {
-            throw new NotImplementedException();
+            var entity = _mapper.Map<Spot>(spot);
+            await _dataStore.UpdateAsync(entity);
         }
 
-        public Task<SpotDto?> GetSpotAsync(CoordinateDto coordinate)
+        public async Task<IEnumerable<SpotDto?>> GetAsync(CoordinateDto coordinate, int radius)
         {
-            throw new NotImplementedException();
+            var query = await _dataStore.GetAllAsync();
+            // TODO: radius limit
+            return query.ToList().Select(x => _mapper.Map<SpotDto>(x));
+        }
+
+        public async Task<SpotDto?> GetSpotAsync(CoordinateDto coordinate)
+        {
+            var coord = _mapper.Map<Coordinate>(coordinate);
+            var spot = await _dataStore.GetAsync(coord);
+            return spot is null ? null : _mapper.Map<SpotDto>(spot);
         }
     }
 }
